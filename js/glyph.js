0e1485eb4c98e583472cfa6bcc93ca3a1a20aa26
TAKANO Mitsuhiro <takano32@gmail.com>
2014-03-05 01:56:14 +0900

    draw 'Clear All' and 'Chaos' glyphs.

--- js/glyph.js
+++ js/glyph.js
@@ -1,57 +1,117 @@
-onload = function() {
-	draw();
+var Position = function(x, y) {
+	this.x = x;
+	this.y = y;
 };
-function draw() {
+
+var positions = [];
+
+function initializePositions() {
+	x = 320;
+	y = 240;
+	positions[0] = new Position(x, y);
+
+	y -= 200;
+	positions[1] = new Position(x, y);
+
+	x -= 100 * Math.sqrt(3)
+	y += 100;
+	positions[2] = new Position(x, y);
+
+	y += 200;
+	positions[3] = new Position(x, y);
+
+	x += 100 * Math.sqrt(3)
+	y += 100;
+	positions[4] = new Position(x, y);
+
+	x += 100 * Math.sqrt(3)
+	y -= 100;
+	positions[5] = new Position(x, y);
+
+	y -= 200;
+	positions[6] = new Position(x, y);
+
+	x = (positions[0].x + positions[2].x) / 2;
+	y = (positions[0].y + positions[2].y) / 2;
+	positions[7] = new Position(x, y);
+
+	x = (positions[0].x + positions[3].x) / 2;
+	y = (positions[0].y + positions[3].y) / 2;
+	positions[8] = new Position(x, y);
+
+	x = (positions[0].x + positions[5].x) / 2;
+	y = (positions[0].y + positions[5].y) / 2;
+	positions[9] = new Position(x, y);
+
+	x = (positions[0].x + positions[6].x) / 2;
+	y = (positions[0].y + positions[6].y) / 2;
+	positions[10] = new Position(x, y);
+}
+
+initializePositions();
+
+onload = function() {
+	drawPoints();
+	// drawClearAllGlyph();
+	drawChaosGlyph();
+	// drawClear();
+};
+
+function drawPoints() {
 	var canvas = document.getElementById('glyph-canvas');
 	if ( ! canvas || ! canvas.getContext ) {
 		return false;
 	}
 	var ctx = canvas.getContext('2d');
-	var x = 320;
-	var y = 240;
-	var r = 10;
-	ctx.beginPath();
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
 
-	ctx.beginPath();
-	y -= 200;
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
+	for (var index in positions) {
+		var r = 10;
+		var pos = positions[index];
+		console.log(pos);
+		ctx.beginPath();
+		ctx.arc(pos.x, pos.y, r, Math.PI*2, false);
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
 
-	ctx.beginPath();
-	x -= 100 * Math.sqrt(3)
-	y += 100;
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
+function drawClearAllGlyph() {
+	var indexes = [1, 2, 3, 4, 5, 6, 1];
+	drawGlyph(indexes);
+}
 
-	ctx.beginPath();
-	y += 200;
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
+function drawChaosGlyph() {
+	var indexes = [3, 2, 1, 6, 10, 0, 8, 4];
+	drawGlyph(indexes);
+}
 
+function drawGlyph(indexes) {
+	var pos = positions;
+	var canvas = document.getElementById('glyph-canvas');
+	if (!canvas || !canvas.getContext) {
+		return false;
+	}
+	var ctx = canvas.getContext('2d');
 	ctx.beginPath();
-	x += 100 * Math.sqrt(3)
-	y += 100;
-	ctx.arc(x, y, r, Math.PI*2, false);
+	var move = false;
+	for (var index in indexes) {
+		var i = indexes[index];
+		if (move == false) {
+			ctx.moveTo(pos[i].x, pos[i].y);
+			move = true;
+		} else {
+			ctx.lineTo(pos[i].x, pos[i].y);
+		}
+	}
 	ctx.stroke();
-	ctx.closePath();
+}
 
-	ctx.beginPath();
-	x += 100 * Math.sqrt(3)
-	y -= 100;
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
-
-	ctx.beginPath();
-	y -= 200;
-	ctx.arc(x, y, r, Math.PI*2, false);
-	ctx.stroke();
-	ctx.closePath();
+function drawClear() {
+	var canvas = document.getElementById('glyph-canvas');
+	if (!canvas || !canvas.getContext) {
+		return false;
+	}
+	var ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, 640, 480);
 }
 
